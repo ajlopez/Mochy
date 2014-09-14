c458eed29611c3b0e98e51fad79058f718826b89
ajlopez <webmaster@ajlopez.com>
2014-09-14 09:44:54 -0300

    Step event, listen on new suites

--- test/events.js
+++ test/events.js
@@ -1,7 +1,7 @@
 
 var suite = require('../lib/suite');
 
-exports['Describe emit event'] = function (test) {
+exports['describe emits event'] = function (test) {
     test.async();
     
     var s = suite();
@@ -18,5 +18,35 @@ exports['Describe emit event'] = function (test) {
         test.equal(events.length, 1);
         test.equal(events[0], 'my suite');
         test.done();
+    });
+}
+
+exports['it and testobj emit events'] = function (testobj) {
+    testobj.async();
+    
+    var s = suite();
+    var events = [];
+    
+    s.describe('my suite', function () {    
+        it('my it', function (done) {
+            done();
+        });
+        
+        test('my test', function (done) {
+            done();
+        });
+    });
+    
+    s.on('suite', function (name) { events.push(name); });
+    s.on('step', function (name) { events.push(name); });
+    
+    s.run(function (err, data) {
+        testobj.equal(err, null);
+        testobj.equal(data, null);
+        testobj.equal(events.length, 3);
+        testobj.equal(events[0], 'my suite');
+        testobj.equal(events[1], 'my it');
+        testobj.equal(events[2], 'my test');
+        testobj.done();
     });
 }
